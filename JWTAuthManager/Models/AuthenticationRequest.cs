e60c4d57fba62ae2b35c19ba513982577cabe354
Prince Asihene <113197395+orgm-pasihene@users.noreply.github.com>
2023-01-18 13:51:00 +0000

    Delete JWTAuthManager directory

--- JWTAuthManager/Models/AuthenticationRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JWTAuthManager.Models
-{
-    public class AuthenticationRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
